5b2b0190973a6d262bd284c8c61f2ee8daca1166
luis granda aguilar <luisgranda107@gmail.com>
2019-05-09 18:27:40 -0500

    Parte 02

--- manejoswitch/src/manejoswitch/UsoSwitch.java
+++ manejoswitch/src/manejoswitch/UsoSwitch.java
@@ -10,5 +10,19 @@ package manejoswitch;
  * @author Granda
  */
 public class UsoSwitch {
-    
+    public static void main(String[] args){
+        //Todo code applications logic here
+        
+        String cadena = "Martes1";
+        switch(cadena){
+            case "Lunes":
+                System.out.printf("Día %s", cadena);
+                break;
+            case "Martes":
+                System.out.printf("Día %s", cadena);
+                break;
+            default:
+            System.out.printf("Ninguna opcion es encontrada");    
+        }
+    }
 }
--- manejoswitch/src/manejoswitch/UsoSwitch3.java
+++ manejoswitch/src/manejoswitch/UsoSwitch3.java
@@ -10,5 +10,25 @@ package manejoswitch;
  * @author Granda
  */
 public class UsoSwitch3 {
-    
+    public static void main(String[] args){
+        //Todo code applications logic here
+        
+        String nombre = "esmeraldAS";
+        char valor = nombre.charAt(0);
+        
+        switch(valor){
+            case 'a':
+            case 'A':
+                System.out.printf("Nombre con inicial %s de %s\n",
+                        valor, nombre.toUpperCase());
+                break;
+            case 'e':
+                System.out.printf("Nombre con inicial %s de %s\n",
+                        valor, nombre.toLowerCase());
+                break;
+                
+            default:
+                System.out.println("Ninguna de las anteriores");
+        }
+    }
 }
